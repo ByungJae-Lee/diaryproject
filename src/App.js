3c93fc42582a6d543e35474a337c16c806b07557
banya <bjgeumgang@gmail.com>
2023-10-10 20:12:09 +0900

    DiaryEditor 추가

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <DiaryEditor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DiaryEditor.js
@@ -0,0 +1,8 @@
+const DiaryEditor = () => {
+  return (
+    <div className="DiaryEditor">
+      <h2>테스트</h2>
+    </div>
+  );
+};
+export default DiaryEditor;
